8a57f6d77f7ae01ab9bb74c02b8ab22ecfe1fb6d
sunil20dhakad <sunil.dhakad@elucidata.io>
2020-01-03 15:43:12 +0530

    [WIP]: change compute pca so that it can filter genes/compounds. Also taken from Shashank's code

--- PollyCommonR/R/compute_pca.R
+++ PollyCommonR/R/compute_pca.R
@@ -6,24 +6,27 @@
 #'
 #' @return A list of loadings and pc values
 #' @export
-compute_pca <- function(sample_raw_mat) {
+compute_pca <- function(sample_raw_mat, num = NULL) {
   message("Compute PCA Started...")
   require(stats)
   require(matrixStats)
-
+  
   variancerow <- matrixStats::rowVars(as.matrix(sample_raw_mat))
-  sample_raw_mat <- sample_raw_mat[!(variancerow <= 0.01),]
+  sample_raw_mat <- sample_raw_mat[!(variancerow == 0.01),]
   sample_raw_mat <- sample_raw_mat[!apply(sample_raw_mat, 1, anyNA), ]
-  data_mat <- as.data.frame(t(sample_raw_mat))
+  
+  if (!identical(num, NULL)){
+    temp_df <- apply(sample_raw_mat, 1, function(x) mad(x))
+    sample_raw_mat <- as.data.frame(cbind(sample_raw_mat,temp_df))
+    sample_raw_mat <- sample_raw_mat[order(-sample_raw_mat$temp_df),]
+    data_mat <- as.data.frame(t(head(sample_raw_mat[, !(colnames(sample_raw_mat) %in% c("temp_df"))], num)))
+  }else{
+    data_mat <- as.data.frame(t(sample_raw_mat))
+  }
+  
   pca_result <- stats::prcomp(data_mat, scale = T)
-
-  loadings <- data.frame(pca_result$x,
-    cohort = row.names(pca_result$x)
-  )
-  eigs <- pca_result$sdev^2
-  var_exp <- eigs / sum(eigs)
-  var_exp <- var_exp * 100
-
+  PCAObj_Summary <- summary(pca_result)
+  
   message("Compute PCA Completed...")
-  return(list(loadings = loadings, var_exp = var_exp))
+  return (PCAObj_Summary)
 }
