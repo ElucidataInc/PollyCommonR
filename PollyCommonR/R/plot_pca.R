ed9e47cda5cdf191e836dc02c37e8c6d7a5d29fc
sunil20dhakad <sunil.dhakad@elucidata.io>
2020-01-03 15:27:51 +0530

    [WIP]: change pca lot function copied from Shashank's code

--- PollyCommonR/R/plot_pca.R
+++ PollyCommonR/R/plot_pca.R
@@ -11,21 +11,51 @@
 #'
 #' @return plotly object
 #' @export
-plot_pca <- function(pca_loadings_var_list, metadata, condition, pc_x, pc_y) {
+plot_pca <- function(PCAObj_Summary, metadata, condition, pc_x, pc_y, interactive = TRUE, pca_cohort_text_format= 'bold', pca_cohort_text_align= "right",
+                     pca_cohort_title_size= 18, pca_cohort_sample_size= 15, pca_plot_axis_format= 'bold', pca_plot_axis_text_size= 14) {
   message("Plot PCA Started...")
+  require(stringr)
   require(plotly)
-
-  cohort_vector <- metadata[, condition]
-
-  xaxis = paste("PC", pc_x, " (", round(pca_loadings_var_list$var_exp[pc_x], 2), "%)", sep = "")
-  yaxis = paste("PC", pc_y, " (", round(pca_loadings_var_list$var_exp[pc_y], 2), "%)", sep = "")
-
-  p <- qplot(x = pca_loadings_var_list$loadings[, pc_x], y = pca_loadings_var_list$loadings[, pc_y], color = cohort_vector, main = "PCA Plot", xlab = xaxis, ylab = yaxis) +
-  stat_ellipse(geom = "polygon", alpha = 1/2, aes(fill = cohort_vector))
-
-  p <- ggplotly(p)
-
+  require(ggsci)
+  
+  pca_var_df <- as.data.frame(PCAObj_Summary$x)
+  pca_var_df$variable <- rownames(pca_var_df) 
+  comb_pca_metadata <- merge(pca_var_df, metadata_df, by.x = 'variable', by.y = 1, sort = FALSE)
+  
+  p <- ggplot(comb_pca_metadata, aes(x = eval(parse(text=paste("PC", pc_x, sep = ""))),
+                                     y = eval(parse(text=paste("PC", pc_y, sep = ""))),
+                                     text = variable, group = comb_pca_metadata[,condition],
+                                     fill = comb_pca_metadata[,condition]
+  )) + # calls the ggplot function with dose on the x-axis and len on the y-axis
+    geom_point(shape = 21, size = 6, alpha = 0.7) + # scatter plot function with shape of points defined as 21 scale.
+    labs(x = paste("PC",pc_x, '(', round(PCAObj_Summary$importance[2,pc_x]*100, 2), '%)'),
+         y = paste("PC",pc_y, '(', round(PCAObj_Summary$importance[2,pc_y]*100, 2), '%)')) + # x and y axis labels
+    guides(color=FALSE, fill = guide_legend('Cohort'))+
+    scale_color_aaas() + # filling the point colors
+    theme(legend.position = pca_cohort_text_align, legend.direction = "vertical", # legend positioned at the bottom, horizantal direction,
+          axis.line = element_line(size = 1, colour = "black"), # axis line of size 1 inch in black color
+          panel.grid.major = element_blank(), # major grids included
+          panel.grid.minor = element_blank(), # no minor grids
+          panel.border = element_blank(), panel.background = element_blank(), # no borders and background color
+          axis.title = element_text(colour="black", size = 18, face = pca_plot_axis_format), # axis title
+          axis.text.x = element_text(colour="black", size = pca_plot_axis_text_size, margin=unit(c(0.5,0.5,0.1,0.1), "cm"), face = pca_plot_axis_format), # x-axis text in fontsize 10
+          axis.text.y = element_text(colour="black", size = pca_plot_axis_text_size, margin=unit(c(0.5,0.5,0.1,0.1), "cm"), face = pca_plot_axis_format), # y-axis text in fontsize 10
+          legend.text = element_text(size = pca_cohort_sample_size, face = pca_cohort_text_format),
+          legend.title = element_text(colour="black", size= pca_cohort_title_size, face= pca_cohort_text_format),
+          axis.ticks.length = unit(-0.25, "cm"))
+  
+  if (interactive == TRUE){
+    p <- ggplotly(p, tooltip = "text") %>% layout(hovermode = "closest")
+    for (cohort_index in 1:length(p$x$data)){
+      name <- p$x$data[[cohort_index]]$name
+      cohort <- strsplit(gsub(re, "\\1", stringr::str_extract_all(name, "\\(([^()]+)\\)")[[1]]),",")[[1]][1]
+      p$x$data[[cohort_index]]$name <- cohort
+    }
+  }
   message("Plot PCA Completed...")
-
+  
   return(p)
 }
+
+p <- plot_pca(pca_cal, metadata_df, 'Cohort', 1,2, interactive = TRUE)
+
