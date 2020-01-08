6a73f5d0e197977a0ca428ace74839783b6ac562
sunil20dhakad <sunil.dhakad@elucidata.io>
2020-01-08 15:41:25 +0530

    [WIP]: give option to scale the data before running prcomp

--- PollyCommonR/R/compute_pca.R
+++ PollyCommonR/R/compute_pca.R
@@ -4,11 +4,19 @@
 #'
 #' @param sample_raw_mat matrix/dataframe containing raw values.
 #' @param num Number of genes/metabolites to be used for pca.
+#' @param center a logical value indicating whether the variables should be 
+#' shifted to be zero centered. Alternately, a vector of length equal the 
+#' number of columns of x can be supplied. The value is passed to scale.
+#' @param scale a logical value indicating whether the variables should be 
+#' scaled to have unit variance before the analysis takes place. The default
+#'  is FALSE for consistency with S, but in general scaling is advisable. 
+#'  Alternatively, a vector of length equal the number of columns of x can 
+#'  be supplied. The value is passed to scale (base function).
 #' @return A list of loadings and pc values
 #' @examples
 #' compute_pca(sample_raw_mat, num = 1000)
 #' @export
-compute_pca <- function(sample_raw_mat = NULL, num = NULL) {
+compute_pca <- function(sample_raw_mat = NULL, num = NULL, center = FALSE, scale = TRUE) {
   message("Compute PCA Started...")
   require(matrixStats)
   require(stats)
@@ -26,7 +34,7 @@ compute_pca <- function(sample_raw_mat = NULL, num = NULL) {
     data_mat <- as.data.frame(t(sample_raw_mat))
   }
   
-  pca_result <- stats::prcomp(data_mat, scale = T)
+  pca_result <- stats::prcomp(data_mat, center = center, scale = scale)
   PCAObj_Summary <- summary(pca_result)
   
   message("Compute PCA Completed...")
