e25cb63938acbc9937a51538b6109fa3ca04b547
sunil20dhakad <sunil.dhakad@elucidata.io>
2020-01-27 14:22:42 +0530

    [WIP]: ad cov calculation and boxplot function

---
+++ PollyCommonR/R/calculate_cohortwise_cov.R
@@ -0,0 +1,46 @@
+#' calculate_cohortwise_cov
+#'
+#' calcualte coefficient of variation across all cohorts
+#'
+#' @param raw_matrix matrix/dataframe containing raw values.
+#' @param metadata dataframe containing metadata information 
+#' @param cohort_col A metadata column where cohorts are present
+#' @return A dataframe with calculated cov
+#' @examples
+#' calculate_cohortwise_cov(raw_matrix, metadata, cohort_col='Cohort')
+#' @import dplyr matrixStats
+#' @export
+calculate_cohortwise_cov <- function(raw_matrix, metadata, cohort_col='Cohort'){
+  
+  message("Calculate Coefficient of Variation Started...")
+  require(dplyr)
+  require(matrixStats)
+  
+  raw_matrix_samples <- colnames(raw_matrix)
+  metadata_samples <- as.character(metadata[, 1])
+  common_samples <- intersect(metadata_samples, raw_matrix_samples)
+  if (length(common_samples) == 0){
+    warning("No common samples in matrix and metadata")
+    return(NULL)
+  }
+  metadata <- dplyr::filter(metadata, !!(sym(colnames(metadata)[1])) %in% !!common_samples)
+  id_cols_vec <- raw_matrix_samples[!raw_matrix_samples %in% common_samples]
+  cohort_vec <- as.character(unique(metadata[[cohort_col]]))
+  cohortwise_cov_list <- list()
+  for (cohort in cohort_vec){
+    interm_df = data.frame(raw_matrix[,id_cols_vec])
+    interm_df[['id']] <- rownames(raw_matrix)
+    interm_df[['cohort']] = cohort
+    cohort_samples <- as.character(dplyr::filter(metadata, !!(sym(cohort_col)) %in% !!cohort)[[1]])
+    cohortwise_sample_mat = as.matrix(raw_matrix[,cohort_samples])
+    interm_df[['mean']] <- rowMeans(cohortwise_sample_mat)
+    interm_df[['std']] <- matrixStats::rowSds(cohortwise_sample_mat)
+    interm_df[['cv']] <- abs(interm_df[['std']]/interm_df[['mean']])*100
+    cohortwise_cov_list[[cohort]] <- interm_df
+  }
+  cohortwise_cov_df <- dplyr::bind_rows(cohortwise_cov_list)
+  
+  message("Calculate Coefficient of Variation Completed...")
+  
+  return(cohortwise_cov_df)
+}
---
+++ PollyCommonR/R/create_cohortwise_cov_boxplot.R
@@ -0,0 +1,53 @@
+#' create_cohortwise_cov_boxplot
+#'
+#' Makes boxplot for cov values across cohorts
+#'
+#' @param calculated_cov_df A dataframe with calculated cov
+#' @param cohorts_order The order of cohorts
+#' @return A plotly object
+#' @examples
+#' create_cohortwise_cov_boxplot(calculated_cov_df, cohorts_order = c('CohortA','CohortB'))
+#' @import dplyr stringr plotly
+#' @export
+create_cohortwise_cov_boxplot <- function(calculated_cov_df, cohorts_order = NULL){
+  
+  message("Create Coefficient of Variation Boxplot Started...")
+  require(dplyr)
+  require(stringr)
+  require(plotly)
+  
+  calculate_cov_df[['cohort']] <- stringr::str_trim(calculate_cov_df[['cohort']])
+  cohorts_vec <- unique(calculated_cov_df[['cohort']])
+  filtered_cohorts_vec <- cohorts_vec
+  common_cohorts <- c()
+  if (length(cohorts_order) != 0){
+    cohorts_order <- stringr::str_trim(cohorts_order)
+    common_cohorts <- intersect(cohorts_order, cohorts_vec)
+  }
+  if (length(common_cohorts) != 0){
+    filtered_cohorts_vec = common_cohorts
+  }
+  calculated_cov_df_filtered <-  calculated_cov_df %>% dplyr::filter(cohort %in% filtered_cohorts_vec)
+  
+  p <- plot_ly(calculated_cov_df_filtered, y = ~cv, color = ~cohort, type = "box") %>%
+    layout(
+      title = "CV Distribution across Cohorts",
+      xaxis = list(
+        title = "Cohorts",
+        titlefont = list(size=16),
+        categoryorder = "array",
+        categoryarray = cohorts_order
+      ),
+      yaxis = list(title = "Coefficient of Variation (%)", titlefont = list(size=16)),
+      showlegend = FALSE
+    ) %>% plotly::config(displaylogo = FALSE,
+                         modeBarButtons = list(list("zoomIn2d"), 
+                                               list("zoomOut2d"), 
+                                               list('toImage')),
+                         mathjax = 'cdn')
+  
+  message("Create Coefficient of Variation Boxplot Completed...")
+  
+  return (p)
+  
+}
