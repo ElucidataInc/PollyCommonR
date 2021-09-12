10aadcf0f0292e8f4d3726b6bac32e698cb1eaae
sunil20dhakad <sunil.dhakad@elucidata.io>
2021-09-12 15:33:09 +0530

    [FIX]: fix marker_expr issue

--- PollyCommonR/R/plot_volcano_from_limma.R
+++ PollyCommonR/R/plot_volcano_from_limma.R
@@ -284,10 +284,10 @@ plot_volcano_from_limma <- function(diff_exp = NULL, log2fc_range = 1, p_val_cut
     }
   }
   
-  ave_expr <- as.numeric(diff_exp[, marker_expr_col])
-  ave_expr <- ave_expr[is.finite(ave_expr)]
-  ave_expr_range <- c(min(ave_expr), max(ave_expr))
   if (identical(marker_size_by_expr, TRUE)){
+    ave_expr <- as.numeric(diff_exp[, marker_expr_col])
+    ave_expr <- ave_expr[is.finite(ave_expr)]
+    ave_expr_range <- c(min(ave_expr), max(ave_expr))
     slope_m <- (marker_size_range[2] - marker_size_range[1]) / (ave_expr_range[2] - ave_expr_range[1])
     eq_constant <- marker_size_range[2] - (slope_m * ave_expr_range[2])                                           
     diff_exp$marker_size <- sapply(diff_exp[, marker_expr_col], function(x) {
@@ -420,7 +420,7 @@ plot_volcano_from_limma <- function(diff_exp = NULL, log2fc_range = 1, p_val_cut
     if (identical(x_label, NULL)){ x_label <- xaxis_lab_gg }
     if (identical(y_label, NULL)){ y_label <- yaxis_lab_gg }
     
-    diff_exp[!is.finite(diff_exp[, marker_expr_col]), marker_expr_col] <- ave_expr_range[1]
+    if (identical(marker_size_by_expr, TRUE)){ diff_exp[!is.finite(diff_exp[, marker_expr_col]), marker_expr_col] <- ave_expr_range[1]}
     p <- ggplot(diff_exp, aes_string(x = x_col, y = y_col, color = "threshold", fill = "threshold", shape = category_col), text = id)
     if (identical(marker_size_by_expr, TRUE)){
       p <- p + geom_point(aes_string(size = marker_expr_col), alpha = marker_opacity) + 
